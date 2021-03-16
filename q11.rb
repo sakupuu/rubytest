11da7a2244aa89effc1ffd91f31a89dcd3dbc933
sakupuu <saku.yuzu.113@gmail.com>
2021-03-16 16:21:40 +0900

    途中保存です

--- q11.rb
+++ q11.rb
@@ -1,3 +1,16 @@
 #11. 10で作成したプログラムをcase文を使い、作りかえてください。
 
 
+countries = ["japan", "us", "italy"]
+countries.each do |country|
+  case country
+  when "japan"
+    puts "#{country}:こんにちは"
+  when "us"
+    puts "#{country}:Hello"
+  when "italy"
+    puts "#{country}:ciao"
+  else 
+    puts "対応する要素がありません"
+  end
+end
