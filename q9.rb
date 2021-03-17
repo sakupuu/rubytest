cde2853f22970e540892b7de85ee0fe7a3c7ddab
sakupuu <saku.yuzu.113@gmail.com>
2021-03-17 12:56:34 +0900

    q9. 修正版です！

--- q9.rb
+++ q9.rb
@@ -3,11 +3,16 @@
 #その後、好きな名前をインスタンスに入れて作成し、作ったクラスメソッドを呼び出してください。（変数展開、アクセサメソッド、イニシャライズメソッド、などいろいろ調べる必要があります。）
 
 class Dog
-  def initialize
-    @name = name
-  end
   attr_accessor :name
+  def initialize(name)
+    self.name = name
+  end
+  
+  def speak
+    puts "ワン！僕の名前は#{self.name}です！"
+  end
 end
 
-name = "poo"
-puts "ワン！僕の名前は#{name}です！"
+dogname = Dog.new("poo")
+dogname.speak
+
